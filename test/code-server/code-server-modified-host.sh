64d4c68fce604b3cf180340b777376aa2378e099
Danielle Maywood <danielle@themaywoods.com>
2025-04-08 10:28:00 +0000

    fix: run code-server as the appropriate user
    Rather than run code-server as root, we instead run it as the
    appropriate user.

--- test/code-server/code-server-modified-host.sh
+++ test/code-server/code-server-modified-host.sh
@@ -7,7 +7,7 @@ source dev-container-features-test-lib
 # Feature-specific tests
 check "code-server version" code-server --version
 check "code-server running" pgrep -f 'code-server/lib/node.*/code-server'
-check "code-server listening" sudo lsof -i "@0.0.0.0:8080"
+check "code-server listening" lsof -i "@0.0.0.0:8080"
 
 # Report results
 reportResults
