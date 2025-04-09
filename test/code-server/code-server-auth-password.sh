ad16d4286003de01944776dbfdda2128aeb5f15d
Danielle Maywood <danielle@themaywoods.com>
2025-04-09 10:27:39 +0000

    feat: allow modifying auth mode
    Adds an auth option to code-server feature to allow modifying the auto
    mode of code-server.

---
+++ test/code-server/code-server-auth-password.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+# Optional: Import test library bundled with the devcontainer CLI
+source dev-container-features-test-lib
+
+# Feature-specific tests
+check "code-server version" code-server --version
+check "code-server running" pgrep -f 'code-server/lib/node.*/code-server'
+check "code-server listening" lsof -i "@127.0.0.1:8080"
+
+entrypoint=$(cat /usr/local/bin/code-server-entrypoint)
+echo "$entrypoint"
+check "code-server auth" grep $'\'code-server.* --auth "password" .*\'' <<<"$entrypoint"
+
+# Report results
+reportResults
