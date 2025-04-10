6f188686eda794e43b8d4b99fa51cf9c75b4201b
Danielle Maywood <danielle@themaywoods.com>
2025-04-10 10:54:54 +0100

    feat: allow customizing workspace directory (#2)
    Allow specifying where code-server should be launched into.

--- src/code-server/install.sh
+++ src/code-server/install.sh
@@ -3,7 +3,7 @@ set -e
 
 CODE_SERVER_INSTALL_ARGS=""
 
-if [ -n "$VERSION" ]; then
+if [[ -n $VERSION ]]; then
 	CODE_SERVER_INSTALL_ARGS="$CODE_SERVER_INSTALL_ARGS --version=\"$VERSION\""
 fi
 
@@ -17,12 +17,18 @@ do
     code-server --install-extension "$extension"
 done
 
+CODE_SERVER_WORKSPACE="$_REMOTE_USER_HOME"
+
+if [[ -n $WORKSPACE ]]; then
+    CODE_SERVER_WORKSPACE="$WORKSPACE"
+fi
+
 cat > /usr/local/bin/code-server-entrypoint \
 << EOF
 #!/usr/bin/env bash
 set -e
 
-su $_REMOTE_USER -c 'code-server --bind-addr "$HOST:$PORT" \$ARGS'
+su $_REMOTE_USER -c 'code-server --bind-addr "$HOST:$PORT" "$CODE_SERVER_WORKSPACE"'
 EOF
 
 chmod +x /usr/local/bin/code-server-entrypoint
