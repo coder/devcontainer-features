0eda689d093ef9a046d2169b8001104e37a86a01
Danielle Maywood <danielle@themaywoods.com>
2025-04-14 11:28:12 +0000

    chore: use array instead of string

--- src/code-server/install.sh
+++ src/code-server/install.sh
@@ -23,34 +23,34 @@ if [[ -n $WORKSPACE ]]; then
     CODE_SERVER_WORKSPACE="$WORKSPACE"
 fi
 
-DISABLE_FLAGS=""
+DISABLE_FLAGS=()
 
 if [[ "$DISABLEFILEDOWNLOADS" == "true" ]]; then
-    DISABLE_FLAGS="$DISABLE_FLAGS --disable-file-downloads"
+    DISABLE_FLAGS+=(--disable-file-downloads)
 fi
 
 if [[ "$DISABLEFILEUPLOADS" == "true" ]]; then
-    DISABLE_FLAGS="$DISABLE_FLAGS --disable-file-uploads"
+    DISABLE_FLAGS+=(--disable-file-uploads)
 fi
 
 if [[ "$DISABLEGETTINGSTARTEDOVERRIDE" == "true" ]]; then
-    DISABLE_FLAGS="$DISABLE_FLAGS --disable-getting-started-override"
+    DISABLE_FLAGS+=(--disable-getting-started-override)
 fi
 
 if [[ "$DISABLEPROXY" == "true" ]]; then
-    DISABLE_FLAGS="$DISABLE_FLAGS --disable-proxy"
+    DISABLE_FLAGS+=(--disable-proxy)
 fi
 
 if [[ "$DISABLETELEMETRY" == "true" ]]; then
-    DISABLE_FLAGS="$DISABLE_FLAGS --disable-telemetry"
+    DISABLE_FLAGS+=(--disable-telemetry)
 fi
 
 if [[ "$DISABLEUPDATECHECK" == "true" ]]; then
-    DISABLE_FLAGS="$DISABLE_FLAGS --disable-update-check"
+    DISABLE_FLAGS+=(--disable-update-check)
 fi
 
 if [[ "$DISABLEWORKSPACETRUST" == "true" ]]; then
-    DISABLE_FLAGS="$DISABLE_FLAGS --disable-workspace-trust"
+    DISABLE_FLAGS+=(--disable-workspace-trust)
 fi
 
 cat > /usr/local/bin/code-server-entrypoint \
@@ -58,7 +58,7 @@ cat > /usr/local/bin/code-server-entrypoint \
 #!/usr/bin/env bash
 set -e
 
-su $_REMOTE_USER -c 'code-server --auth "$AUTH" --bind-addr "$HOST:$PORT" $DISABLE_FLAGS "$CODE_SERVER_WORKSPACE"'
+su $_REMOTE_USER -c 'code-server --auth "$AUTH" --bind-addr "$HOST:$PORT" ${DISABLE_FLAGS[*]} "$CODE_SERVER_WORKSPACE"'
 EOF
 
 chmod +x /usr/local/bin/code-server-entrypoint
