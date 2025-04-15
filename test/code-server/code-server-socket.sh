394d3d6650e702c9ee44710988d0528ed6723663
Danielle Maywood <danielle@themaywoods.com>
2025-04-15 12:03:05 +0000

    test: re-add socket mode check

--- test/code-server/code-server-socket.sh
+++ test/code-server/code-server-socket.sh
@@ -9,7 +9,7 @@ check "code-server version" code-server --version
 check "code-server running" pgrep -f 'code-server/lib/node.*/code-server'
 
 check "code-server socket" test -S /tmp/code-server.sock
-# check "code-server socket-mode" grep '0755' <<< $(stat /tmp/code-server.sock)
+check "code-server socket-mode" grep '0775' <<< $(stat /tmp/code-server.sock)
 
 # Report results
 reportResults
