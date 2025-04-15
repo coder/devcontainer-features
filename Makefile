a926b94a78238e204f6ed3f0baa554c3172a612d
Danielle Maywood <danielle@themaywoods.com>
2025-04-15 09:43:01 +0100

    feat: support `--socket*` options (#7)
    Add new configuration options:
    
    socket: Path to a socket.
    socketMode: File mode of the socket when using the socket option.

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
 .PHONY: test
 test:
 	devcontainer features test
+
+.PHONY: docs
+docs: src/code-server/README.md
+	cd src && devcontainer features generate-docs -n coder/devcontainer-features
---
+++ scripts/check_unstaged.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+FILES="$(git ls-files --other --modified --exclude-standard)"
+if [[ "$FILES" != "" ]]; then
+	mapfile -t files <<<"$FILES"
+
+	echo
+	echo "The following files contain unstaged changes:"
+	echo
+	for file in "${files[@]}"; do
+		echo "  - $file"
+	done
+
+	echo
+	echo "These are the changes:"
+	echo
+	for file in "${files[@]}"; do
+		git --no-pager diff "$file" 1>&2
+	done
+
+	echo
+	echo "ERROR: Unstaged changes, see above for details."
+  exit 1
+fi
